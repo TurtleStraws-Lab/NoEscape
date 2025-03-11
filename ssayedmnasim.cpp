b3adeb1228f5177713ef7bc08dc2d084b37952aa
SayedJalal SayedMNasim <ssayedmnasim@odin.cs.csubak.edu>
2025-03-11 09:15:50 -0700

    removed main

--- ssayedmnasim.cpp
+++ ssayedmnasim.cpp
@@ -3,10 +3,30 @@
 //
 //
 #include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+#include <ctime>
+#include <cmath>
+#include <X11/Xlib.h>
+//#include <X11/Xutil.h>
+//#include <GL/gl.h>
+//#include <GL/glu.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+#include "log.h"
+#include "fonts.h"
+
 using namespace std;
 
-int main()
+void sayed()
 {
-    cout<<"first commit"<<endl;
-    retrun 0;
+   // Rect r;
+   // r.left =10;
+   // r.center =0;
+
+   // ggprint8b(&r, 16, 0x00ff0000, "Sayed Jalal Sayed M Nasim");
+
+    cout<<"the Game just started";
+   
 }
