9d5d085a5f7475e06d98b8ab444f8347d0582590
devinvas <devin.vasquez225@gmail.com>
2025-03-14 04:18:34 -0700

    header file

---
+++ dvasquez4.h
@@ -0,0 +1,17 @@
+
+#ifndef DVASQUEZ4_H
+#define DVASQUEZ4_H
+//#ifndef STICKMAN_H
+//#define STICKMAN_H
+
+#include <GL/gl.h>
+
+class Ship;
+class Game; 
+extern Ship ship;
+extern Game g;
+
+
+void Stickman(Game &g);
+
+#endif
