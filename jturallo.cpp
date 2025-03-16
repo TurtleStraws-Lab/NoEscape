e0f30d74858216f504f008b8675ed6ec596eb2e1
Eve Turallo <jturallo@odin.cs.csubak.edu>
2025-03-16 01:07:50 -0700

    created textbox class, can be reused for instruction menu

--- jturallo.cpp
+++ jturallo.cpp
@@ -9,41 +9,76 @@
 #include <string.h>
 #include <iostream>
 #include <GL/glx.h>
+#include <GL/gl.h>
 #include "fonts.h"
 #include "jturallo.h"
 
 /*
-currently rendering a background for loading screen and for the
-background of maze
-use the background file rather than repurposing code
-*/
+   currently rendering a background for loading screen and for the
+   background of maze
+   use the background file rather than repurposing code
+   */
 
 
 //should the function end after receiving a signal that
 //loading has finished? or should it have a min timer
 
 /*
-void render_Text(float x, float y, const char* text, void* font) {
+   void render_Text(float x, float y, const char* text, void* font) {
 
-}
-*/
+   }
+   */
 
 
-//will upgrade this to be done with graphics rather than window text soon
-//adding now for the sake of committing code for lab
-void displayCredits() {
+Textbox::Textbox(int x, int y, int width, int height, const std::string &text, unsigned int color)
+    : x(x), y(y), width(width), height(height), text(text), color(color) {}
+
+void Textbox::draw()
+{
     Rect r;
-    glClear(GL_COLOR_BUFFER_BIT);
-    r.bot = 10;
-    //r.bot = gl.yres - 20;
+    r.bot = y;
+    r.left = x;
     r.center = 0;
-    ggprint8b(&r, 16, 0x00ff0000, "Moises Gonzales");
-    ggprint8b(&r, 16, 0x00ff0000, "Evelynn Turallo");
-    ggprint8b(&r, 16, 0x00ff0000, "Geneva Regpala");
-    ggprint8b(&r, 16, 0x00ff0000, "Sayed Jalal Sayed M Nasim");
-    ggprint8b(&r, 16, 0x00ff0000, "Devin Vasquez");
+
+    glClear(GL_COLOR_BUFFER_BIT);
+    ggprint8b(&r, 16, color, text.c_str());
 
 }
+
+void Textbox::setText(const std::string &newText) {
+    text = newText;
+}
+
+void Textbox::setPosition(int newX, int newY) {
+    x = newX;
+    y = newY;
+}
+
+void Textbox::setColor(unsigned int newColor) {
+    color = newColor;
+}
+
+void displayCredits() {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    Textbox title(200, 500, 400, 50, "Game Credits", 0x00ffffff);
+    Textbox name1(200, 450, 400, 50, "Moises Gonzales", 0x00ff0000);
+    Textbox name2(200, 400, 400, 50, "Evelynn Turallo", 0x0000ff00);
+    Textbox name3(200, 350, 400, 50, "Geneva Regpala", 0x000000ff);
+    Textbox name4(200, 300, 400, 50, "Sayed Jalal Sayed M Nasim", 0x00ff00ff);
+    Textbox name5(200, 250, 400, 50, "Devin Vasquez", 0x00ffff00);
+
+    title.draw();
+    name1.draw();
+    name2.draw();
+    name3.draw();
+    name4.draw();
+    name5.draw();
+
+    glFlush();
+}
+
+//textbox class can be reused for an instruction menu
 
 //notes:
 //use small, individual statements that do not
