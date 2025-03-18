439b11db1227a4cbd0d3b084e9a6f5732ffdea2b
Eve Turallo <jturallo@odin.cs.csubak.edu>
2025-03-17 22:04:39 -0700

    fully added background function

--- jturallo.cpp
+++ jturallo.cpp
@@ -74,24 +74,24 @@ void makeCredits() {
     }
 
     if (!names) {
-    std::string creditsText = 
-        "Game Credits\n"
-        "Moises Gonzales\n"
-        "Evelynn Turallo\n"
-        "Geneva Regpala\n"
-        "Sayed Jalal Sayed M Nasim\n"
-        "Devin Vasquez";
+        std::string creditsText = 
+            "Game Credits\n"
+            "Moises Gonzales\n"
+            "Evelynn Turallo\n"
+            "Geneva Regpala\n"
+            "Sayed Jalal Sayed M Nasim\n"
+            "Devin Vasquez";
 
-    names = new Textbox(200, 450, 400, 250, creditsText, 0x0000ff00);
-    //Going to try having all names in one textbox
-    //to minimize # of objects
-    /*
-       Textbox names(200, 450, 400, 50, "Moises Gonzales", 0x00ff0000);
-       Textbox name2(200, 400, 400, 50, "Evelynn Turallo", 0x0000ff00);
-       Textbox name3(200, 350, 400, 50, "Geneva Regpala", 0x000000ff);
-       Textbox name4(200, 300, 400, 50, "Sayed Jalal Sayed M Nasim", 0x00ff00ff);
-       Textbox name5(200, 250, 400, 50, "Devin Vasquez", 0x00ffff00);
-       */
+        names = new Textbox(200, 450, 400, 250, creditsText, 0x0000ff00);
+        //Going to try having all names in one textbox
+        //to minimize # of objects
+        /*
+           Textbox names(200, 450, 400, 50, "Moises Gonzales", 0x00ff0000);
+           Textbox name2(200, 400, 400, 50, "Evelynn Turallo", 0x0000ff00);
+           Textbox name3(200, 350, 400, 50, "Geneva Regpala", 0x000000ff);
+           Textbox name4(200, 300, 400, 50, "Sayed Jalal Sayed M Nasim", 0x00ff00ff);
+           Textbox name5(200, 250, 400, 50, "Devin Vasquez", 0x00ffff00);
+           */
 
     }
 }
@@ -119,6 +119,58 @@ void removeCredits() {
     names = nullptr;
 }
 
+Image::~Image() {
+    delete[] data;
+}
+
+//Taken from background.cpp
+//Variables in jturallo.h
+Image::Image(const char *fname) {
+    if (fname[0] == '\0')
+        return;
+
+    char name[40];
+    strcpy(name, fname);
+    int slen = strlen(name);
+    name[slen - 4] = '\0';
+
+    char ppmname[80];
+    sprintf(ppmname, "%s.ppm", name);
+
+
+    char ts[100];
+    sprintf(ts, "convert %s %s", fname, ppmname);
+    system(ts);
+
+    FILE *fpi = fopen(ppmname, "r");
+    if (fpi) {
+        char line[200];
+        fgets(line, 200, fpi);  
+        fgets(line, 200, fpi);   
+
+
+        while (line[0] == '#' || strlen(line) < 2)
+            fgets(line, 200, fpi);
+
+        sscanf(line, "%i %i", &width, &height);
+        fgets(line, 200, fpi);
+
+
+        int n = width * height * 3;
+        data = new unsigned char[n];
+
+        for (int i = 0; i < n; i++)
+            data[i] = fgetc(fpi);
+
+        fclose(fpi);
+    } else {
+        printf("ERROR opening image: %s\n", ppmname);
+        exit(0);
+    }
+
+
+    unlink(ppmname);
+}
 
 //textbox class can be reused for an instruction menu
 
