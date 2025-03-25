aad2d388a406f7424294db65590ef9b545ea12bc
SayedJalal SayedMNasim <ssayedmnasim@odin.cs.csubak.edu>
2025-03-24 23:59:36 -0700

    changes to file

--- ssayedmnasim.cpp
+++ ssayedmnasim.cpp
@@ -16,6 +16,7 @@
 #include <GL/glx.h>
 #include "log.h"
 #include "fonts.h"
+#include "ssayedmnasim.h"
 
 using namespace std;
 
@@ -29,4 +30,29 @@ void sayed()
 
     cout<<"the Game just started";
    
+}
+
+void changeDirection()
+{
+    // Reset velocity
+//    g.ship.vel[0] = 0.0f;
+//    g.ship.vel[1] = 0.0f;
+
+    // Apply directional movement
+//    if (gl.keys[XK_Up]) {
+//        g.ship.vel[1] = 4.0f;
+//       g.ship.angle = 0.0f;
+//    }
+//    if (gl.keys[XK_Down]) {
+//        g.ship.vel[1] = -4.0f;
+//        g.ship.angle = 180.0f;
+//    }
+//    if (gl.keys[XK_Left]) {
+//        g.ship.vel[0] = -4.0f;
+//        g.ship.angle = 270.0f;
+//    }
+//    if (gl.keys[XK_Right]) {
+//        g.ship.vel[0] = 4.0f;
+//        g.ship.angle = 90.0f;
+//    }
 }
