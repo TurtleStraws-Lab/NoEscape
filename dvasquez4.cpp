88451d922011d0007245c30ccc9ee7f1031731e0
devinvas <devin.vasquez225@gmail.com>
2025-04-01 06:45:19 -0700

    changes to stickman

--- dvasquez4.cpp
+++ dvasquez4.cpp
@@ -3,60 +3,45 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
+#include <cmath>
 #include "dvasquez4.h"
 
-void Stickman() {
+drawCircle::drawCircle(float x2,float y2,float r,int num_seg) {
+	glBegin(GL_LINE_LOOP);
+	for (int i = 0; i < num_seg;i++) {
+		float theta = 2.0f * 3.1415926f * float(i) / float(num_seg);
+		float x = r * cosf(theta);
+		float y = r * sinf(theta);
+		glVertex2f(x + x2, y + y2);
+	}
+}
+
+void DrawStickman::Stickman(float a, float b, float c ,float d) {
+	glClear(GL_COLOR_BUFFER_BIT);
 	glPushMatrix();
-	GLfloat white[3] = {1.0f,1.0f,1.0f};
-	glColor3fv(white);
+	glTranslatef(a,b,c);
+	glRotatef(d,0.0f,0.0f,1.0f);
+	drawCircle(0.0f,0.8f,0.1f,20);
 
-
-	//Draw the head (a simple triangle)
-	glBegin(GL_TRIANGLES);
-	glVertex2f(-12.0f, -10.0f);  // Left part of head
-	glVertex2f(0.0f, 20.0f);     // Top of head
-	glVertex2f(12.0f, -10.0f);  // Right part of head
-	glEnd();
-
+	glBegin(GL_LINES);
 	// Draw the body (a vertical line)
-	glBegin(GL_LINES);
-	glVertex2f(0.0f, -6.0f);  // Bottom of body
-	glVertex2f(0.0f, 20.0f);  // Top of body
+	glVertex2f(0.0f, 0.7f); 
+	glVertex2f(0.0f, 0.4f);
+	//left arm
+	glVertex2f(0.0f, 0.6f);  
+	glVertex2f(-0.2f, 0.5f);
+	//right arm
+	glVertex2f(0.0f, 0.6f);  
+	glVertex2f(0.2f, 0.5f);
+	//left leg
+	glVertex2f(0.0f, 0.4f); 
+	glVertex2f(-0.2f, 0.2f); 
+	//right leg
+	glVertex2f(0.0f, 0.4f); 
+	glVertex2f(10.0f, 0.2f); 
 	glEnd();
 
-	// Draw the arms (two slanted lines)
-	glBegin(GL_LINES);
-	glVertex2f(-12.0f, -6.0f);  // Left arm starting point
-	glVertex2f(12.0f, -6.0f);   // Right arm starting point
-	glEnd();
+	glFlush();
 
-	// Draw the legs (two slanted lines)
-	glBegin(GL_LINES);
-	glVertex2f(0.0f, -6.0f);   // Left leg starting point
-	glVertex2f(-10.0f, -20.0f); // Left leg end point
-	glVertex2f(0.0f, -6.0f);   // Right leg starting point
-	glVertex2f(10.0f, -20.0f); // Right leg end point
-	glEnd();
-
-	/*glPopMatrix();	
-	  glPushMatrix();
-	  glTranslatef(g.ship.pos[0], g.ship.pos[1],g.ship.pos[2]);
-	  glBegin(GL_TRIANGLES);
-	//glVertex2f(-12.0f, -10.0f);
-	//glVertex2f(  0.0f, 20.0f);
-	//glVertex2f( 0.0f, -6.0f);
-	glVertex2f(  -12.0f, -10.0f);
-	glVertex2f(  0.0f,  20.0f);
-	glVertex2f(  0.0f,  -6.0f);
-	glVertex2f(  0.0f,  -6.0f);
-	glVertex2f(  0.0f,  20.0f);
-	glVertex2f( 12.0f, -10.0f);
-	glEnd();
-	glColor3f(1.0,0.0f,0.0f);
-	glBegin(GL_POINTS);
-	glVertex2f(0.0f,0.0f);
-	glEnd();
-	glPopMatrix();	
-	*/
 }
 
--- dvasquez4.h
+++ dvasquez4.h
@@ -3,15 +3,25 @@
 #define DVASQUEZ4_H
 //#ifndef STICKMAN_H
 //#define STICKMAN_H
-
 #include <GL/gl.h>
+#define VecZero(v) (v)[0]=0.0,(v)[1]=0.0,(v)[2]=0.0
+typedef float Flt;
+typedef float Vec[3];
+typedef Flt Matrix[4][4];
+#include <GL/gl.h>
+class drawCircle
+{
+	private:
+		float x,y,r;
+		int num_seg;
+	public:
+		drawCircle(float x, float y, float r, int num_seg);
+};
 
-class Ship;
-class Game; 
-extern Ship ship;
-extern Game g;
-
-
-void Stickman(Game &g);
+class DrawStickman {
+	public:
+		void Stickman(float r, float b, float a, float z);
+	
+};
 
 #endif
